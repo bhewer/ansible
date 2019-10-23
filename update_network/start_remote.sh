f6ed86f69b21e57c64ab794441a1e816d3540179
Benedikt Hewer <benedikt.hewer@math.uni-sb.de>
2019-10-23 14:02:45 +0200

    Revert "changes start_remote"
    This reverts commit 73cf3ef7122784f4a7b8a3c0b724d7dfab10aa10.

--- update_network/start_remote.sh
+++ update_network/start_remote.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ ! -f $1/.ssh/id_apps ] ; then
+if [ ! -f ~/.ssh/id_apps ] ; then
         ssh-keygen -t ed25519 -f id_apps -N '' -C "auto-generated key to access remote apps"
-        cat $1/.ssh/id_apps.pub >> $1/.ssh/authorized_keys
+        cat ~/.ssh/id_apps.pub >> ~/.ssh/authorized_keys
 fi
 pyhoca-cli --server "$2" -c "$3" -q lan --pack 16m-jpeg-9
