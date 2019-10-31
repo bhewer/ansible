be6fcc4952bbff0cac01f540ae9c6f5f4cb1f80f
Benedikt Hewer <benedikt.hewer@math.uni-sb.de>
2019-10-31 14:56:21 +0100

    now uses gap from /opt, enable keyboard autodetect in d9-apps

--- update_network/start_remote.sh
+++ update_network/start_remote.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /usr/local/bin/generate_remoteKey $1
-pyhoca-cli --server "$2" -c "$3" -q lan --pack 16m-jpeg-9
+pyhoca-cli --server "$2" -c "$3" -q lan --pack 16m-jpeg-9 --kbd-type auto -g maximize
